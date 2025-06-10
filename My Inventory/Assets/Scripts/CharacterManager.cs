525299d47760efe59ca4d83511d7c64123d6c1aa
halonine9 <dlselhvxg@gmail.com>
2025-06-10 14:45:21 +0900

    [feat] Step3 UI구성 및 버튼 연결

---
+++ My Inventory/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+   public float Atk {get; private set;}
+   public float Def {get; private set;}
+   public float Hp {get; private set;}
+   public float Critical {get; private set;}
+   
+}
---
+++ My Inventory/Assets/Scripts/UIManager.cs
@@ -0,0 +1,13 @@
+
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text atkText;
+    [SerializeField] TMP_Text defText;
+    [SerializeField] TMP_Text hpText;
+    [SerializeField] TMP_Text criticalText;
+    
+    
+}
